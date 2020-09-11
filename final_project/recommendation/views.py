da2a541c323bb3ad03b6fb67794389c64d0056ea
Jun Nishimura <n.junjun0303@gmail.com>
2020-09-11 22:06:19 +0900

    Add templates for recommendation

--- final_project/recommendation/views.py
+++ final_project/recommendation/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+
+    }
+    return render(request, 'recommendation/index.html', context)
+
+def select(request):
+    context = {
+        'title': 'select',
+    }
+    return render(request, 'recommendation/select.html', context)
+
+def result(request):
+    context = {
+        'title': 'result'
+    }
+    return render(request, 'recommendation/result.html', context)
