cdf1e8723b1eae03b1a6ac2bd95f230f94ec1dab
Jun Nishimura <n.junjun0303@gmail.com>
2020-09-13 21:50:06 +0900

    add recommend system with ajax choose the animes to recommend in the background, and pass them to the fornt page using ajax

--- final_project/recommendation/urls.py
+++ final_project/recommendation/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('select/', views.select, name='select'),
     path('select/search', views.search, name='search'),
-    path('select/search/result', views.search_result, name='search_result'),
+    path('select/search/candidates/', views.candidates, name='candidates'),
+    path('select/search/result/', views.search_result, name='search_result'),
     path('select/category/', views.category, name='category'),
-    path('select/category/result', views.category_result, name='category_result'),
+    path('select/category/result/', views.category_result, name='category_result'),
 ]
--- final_project/recommendation/views.py
+++ final_project/recommendation/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from final_project.settings import BASE_DIR
+import pickle
+import pandas as pd
 
 # Create your views here.
 def index(request):
@@ -19,11 +23,28 @@ def search(request):
     }
     return render(request, 'recommendation/search.html', context)
 
+def candidates(request):
+    result = ''
+    if request.method == 'POST':
+        animeTitle = request.POST.get('animeTitle')
+        try:
+            pickle_path = BASE_DIR + '\machine_learning\\item_sim_df.pickle'
+            with open(pickle_path, 'rb') as f:
+                item_sim_df = pickle.load(f)
+                candidates = item_sim_df.sort_values(by=animeTitle, ascending=False).index[1:11]
+                candidates = candidates.tolist()
+                return JsonResponse({'animeTitle': animeTitle, 
+                                     'candidates': candidates})
+
+        except:
+            print('Error to open a file')
+        return HttpResponse('error to get open pickle file')
+    else:
+        return HttpResponse('error to get post request')
+
+
 def search_result(request):
-    context = {
-        'title': 'search_result',
-    }
-    return render(request, 'recommendation/search_result.html', context)
+    pass
 
 def category(reqeust):
     context = {
